7c1ea8ad471cdc0095fbe7723d743d379cdd266d
mcginnisalexander <mcginnisalexander@gmail.com>
2019-10-25 15:18:56 -0700

    Update red-blue-LED-tracking.py

--- red-blue-LED-tracking.py
+++ red-blue-LED-tracking.py
@@ -1,10 +1,16 @@
-#This runs on the OpenMV H7
-
 
 import sensor, image, time, math
 
 
-thresholdRed = [(64, 100, 14, 127, -128, 127)]
+
+#threshold values were determined by taking a snapshot of the LED test board at a high altitude, and using the Tools > Machine Vision > Threshold Editor
+#thresholdRed = [(64, 100, 14, 127, -128, 127)]
+#thresholdBlue = [(75, 100, -128, 2, -128, 127)]
+
+#thresholdRed = [(80, 100, -6, 50, -18, 3)]
+#thresholdBlue = [(74, 100, 7, 36, -60, -8)]
+
+thresholdRed = [(80, 100, -6, 50, -18, 3)]
 thresholdBlue = [(75, 100, -128, 2, -128, 127)]
 
 sensor.reset()
@@ -16,10 +22,29 @@ sensor.set_auto_gain(False) # must be turned off for color tracking
 sensor.set_auto_whitebal(False) # must be turned off for color tracking
 clock = time.clock()
 
-xpos1 = 0
-ypos1 = 0
-xpos2 = 0
-ypos2 = 0
+#xpos1 = 0
+#ypos1 = 0
+#xpos2 = 0
+#ypos2 = 0
+
+def getthoseblobs(targetColor,img):
+
+    if targetColor == 'red':
+        LABlist = thresholdRed
+        RGBcolor = (255,0,0)
+    if targetColor == 'blue':
+        LABlist = thresholdBlue
+        RGBcolor = (0,0,255)
+    try:
+        for blob in img.find_blobs(LABlist, pixels_threshold=10, area_threshold=10): #red blobs
+            xpos = blob.cx()
+            ypos = blob.cy()
+            img.draw_cross(xpos, ypos)
+            img.draw_circle(blob.enclosing_circle(),color=RGBcolor)
+        return [xpos,ypos]
+    except:
+        return 0
+
 
 # Only blobs that with more pixels than "pixel_threshold" and more area than "area_threshold" are
 # returned by "find_blobs" below. Change "pixels_threshold" and "area_threshold" if you change the
@@ -28,19 +53,9 @@ ypos2 = 0
 while(True):
     clock.tick()
     img = sensor.snapshot()
-    xpos1 = 0
-    ypos1 = 0
-    xpos2 = 0
-    ypos2 = 0
-    for blob in img.find_blobs(thresholdRed, pixels_threshold=10, area_threshold=10):
-        xpos1 = blob.cx()
-        ypos1 = blob.cy()
-        img.draw_cross(xpos1, ypos1)
-    for blob in img.find_blobs(thresholdBlue, pixels_threshold=10, area_threshold=10):
-        xpos2 = blob.cx()
-        ypos2 = blob.cy()
-        img.draw_cross(xpos2, ypos2)
     #print(clock.fps())
-    print('red x,y: ' + str(xpos1) + ',' + str(ypos1))
-    print('blue x,y: ' + str(xpos2) + ',' + str(ypos2))
+    redPosXY = getthoseblobs('red',img)
+    bluePosXY = getthoseblobs('blue',img)
+    print('red x,y: ' + str(redPosXY))
+    print('blue x,y: ' + str(bluePosXY))
 
