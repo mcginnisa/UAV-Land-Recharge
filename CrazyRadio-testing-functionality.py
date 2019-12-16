0bb25067459567ffa2248d5043ba5fee7782ddb9
JosephHaun <riddlemethis565@gmail.com>
2019-12-16 11:42:53 -0800

    Saving changes

--- CrazyRadio-testing-functionality.py
+++ CrazyRadio-testing-functionality.py
@@ -4,8 +4,10 @@ import logging
 import time
 import cflib.crtp
 import random
+
 from cflib.crazyflie import Crazyflie
 from cflib.utils.callbacks import Caller
+from cflib.positioning.motion_commander import MotionCommander
 
 cflib.crtp.init_drivers()
 available = cflib.crtp.scan_interfaces()
@@ -14,21 +16,16 @@ for i in available:
 
 crazyflie = Crazyflie()
 #crazyflie.connected.add_callback(crazyflie_connected)
-crazyflie.open_link("radio://0/80/250K")
+if(len(available) > 0):
+    crazyflie.open_link(available[0][0])
+    with MotionCommander(crazyflie) as UAV:
+        UAV.up(0.5, 0.01)
+        time.sleep(5)
+        UAV.down(0.5, 0.01)
+    crazyflie.close_link()
+    
+else:
+    print("ERROR: Unable to find anything")
 
-roll = 0.0
-pitch = 0.0
-yawrate = 0
-thrust = 0
 
-for i in range(1000):
-    print("Sending Setpoint", (roll, pitch, yawrate, thrust))
-    crazyflie.commander.send_setpoint(0,0,0,0)
-    crazyflie.commander.send_setpoint(roll,pitch,yawrate,10001+i)
-    roll = random.random()
-    pitch = random.random()
-    yawrate = random.random()
-    thrust = 10000*random.random()
-    time.sleep(0.001)
 
-crazyflie.close_link()
