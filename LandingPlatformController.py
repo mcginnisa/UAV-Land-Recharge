dfc007e0c4d2864dd9cc3f1994ce29d58bddc1f7
JosephHaun <riddlemethis565@gmail.com>
2020-01-06 15:12:50 -0800

    Began implementation of serial parsing functionality

--- LandingPlatformController.py
+++ LandingPlatformController.py
@@ -5,30 +5,85 @@ import glob
 
 class LandingPlatformController():
     
-    def __init__(self, UAV=None, cameraInitValue='{900$900}\r\n'):
+    def __init__(self, UAV=None, cameraInitValue='{900$900}\r\n', defaultHoverHeight=0.3, defaultVelocity=0.1):
+        #Define/Manage UAV connection
+        if(UAV == None):
+            print("ERROR: Landing Platform Controller requires a UAV control object")
+            
+        #Define class constants
+        self._uav = UAV
+        self._uavVelocity = defaultVelocity #Find out default velocity
+        self._hoverHeight = defaultHoverHeight
+        self._uavPos = [-1, -1, -1]
+        self._landingPos = [-1, -1, -1] #Will need to be defined somehow
+        self._cameraInitValue = cameraInitvalue
+        
         #Define/Manage Serial connection
         self._camera = serial.Serial(self._getCameraSerialConnection(cameraInitValue))
-        
-        #Define/Manage UAV connection
-        #self._uav = UAVController()
-        self._uavPos = [-1, -1, -1]
 
     def _getUAVPosition(self):
+        """
+        Function: _getUAVPosition
+        Purpose: Get the most up-to-date UAV position in rectangular coordinates
+        Inputs: None
+        Outputs: curPosition - an array of floating point values representing <x, y, z>
+        """
         #Need to figure out serial data
-        pass
+        self._uavPos = [-1, -1 , -1]
+        self._uavPos[2] = self._uavGetHeight()
+        #Parse serial data
+        #Flush camera bus
+        positionString=""
+        while(self._camera.read(1).decode('ascii') != '{'): {""" Do nothing """}
+        while(self._camera.read(1).decode('ascii') != '}'): { positionString = positionString + self._camera.read(1).decode('ascii') }
+        splitIndex=positionString.rfind("%") #Make sure this hard code is changed later
+        pixelX = float(positionString[:splitIndex])
+        pixelY = float(positionString[(splitIndex+1):])
+        return
 
     def _calculateOffset(self):
+        """
+        Function: _calculateOffset
+        Purpose: Calculate the offset distance of the UAV to the desired landing point
+        Inputs: None
+        Outputs: offset - an array of values representing <dx, dy, dz> 
+        """
         offset = [0, 0, 0]
-        curPosition = _getUAVPosition()
-        offset[0] = curPosition[0] - cameraMidPoint[0]
-        offset[1] = curPosition[1] - cameraMidPoint[1]
-        offset[2] = curPosition[2] - cameraMidPoint[2]
+        curPosition = self._getUAVPosition()
+        offset[0] = curPosition[0] - self._landingPos[0]
+        offset[1] = curPosition[1] - self._landingPos[1]
+        offset[2] = curPosition[2] - self._landingPos[2]
         return offset
 
-    def _sendMovement(self):
+    def _uavGetHeight(self):
+        """
+        Function: _uavGetHeight
+        Purpose: Get the UAV z-coordinate in the world frame
+        Inputs: None
+        Outputs: a floating point value representing the z-coordinate 
+        """
+        return self._hoverHeight
+
+    def _sendMovement(self, xpos, ypos, zpos):
+        """
+        Function: _sendMovement
+        Purpose: Instruct the UAV to move to certain coordinates
+        Inputs: xpos - a floating point value denoting the new x position to move towards
+                ypos - a floating point value denoting the new y position to move towards
+                zpos - a floating point value denoting the new z position to move towards
+        Outputs: None
+        """
+        self.uav.move(xpos, ypos, zpos, self._uavVelocity)
         pass
 
     def _performLandingSequence(self):
+        """
+        Function: _performLandingSequence
+        Purpose: Instruct the UAV to land fully once centered
+        Inputs: None
+        Outputs: None
+        """
+        self.uav.land()
         pass
 
     def _getBatteryLevel(self):        
@@ -83,3 +138,6 @@ class LandingPlatformController():
                 pass
         #End of Function
         return availablePorts
+
+if True:
+    Test = LandingPlatformController()
